e4b2af9fcf0f6dd974bef39309f4b25d208692af
Arthur0610 <arthur0610@163.com>
2020-10-26 13:53:03 +0800

    update W04

---
+++ FIT5032_W04_ModelFirst/FIT5032_W04_ModelFirst/Models/FIT5032_W04_Model.edmx.sql
@@ -0,0 +1,84 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 10/13/2020 10:49:05
+-- Generated from EDMX file: E:\Monash\FIT5032Workshop\FIT5032_W04_ModelFirst\FIT5032_W04_ModelFirst\Models\FIT5032_W04_Model.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [FIT5032_W04_ModelFirst];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'StudentSet'
+CREATE TABLE [dbo].[StudentSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [LastName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'UnitSet'
+CREATE TABLE [dbo].[UnitSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [StudentId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'StudentSet'
+ALTER TABLE [dbo].[StudentSet]
+ADD CONSTRAINT [PK_StudentSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'UnitSet'
+ALTER TABLE [dbo].[UnitSet]
+ADD CONSTRAINT [PK_UnitSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [StudentId] in table 'UnitSet'
+ALTER TABLE [dbo].[UnitSet]
+ADD CONSTRAINT [FK_StudentUnit]
+    FOREIGN KEY ([StudentId])
+    REFERENCES [dbo].[StudentSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_StudentUnit'
+CREATE INDEX [IX_FK_StudentUnit]
+ON [dbo].[UnitSet]
+    ([StudentId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
