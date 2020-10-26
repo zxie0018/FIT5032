5d598cbb46304509633f133ab5ab707543b63060
Arthur0610 <arthur0610@163.com>
2020-10-26 13:44:41 +0800

    update W03

---
+++ FIT5032_Week03/FIT5032_Week03/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using FIT5032_Week03.Exercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            HelloWorld.Hello hello = new HelloWorld.Hello();
+
+            ViewBag.Message = hello.GetHello();
+
+            ExampleDictionary ed = new ExampleDictionary();
+
+            ed.Example();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
